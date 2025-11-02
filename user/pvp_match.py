12267bcd23134c9f4ec035ffa8a87a2fd58df50a
HussainE4 <hussain.elahi@mail.utoronto.ca>
2025-11-02 00:58:26 -0400

    a

--- user/pvp_match.py
+++ user/pvp_match.py
@@ -1,14 +1,21 @@
 from environment.environment import RenderMode, CameraResolution
 from environment.agent import run_real_time_match
-from user.train_agent import UserInputAgent, BasedAgent, ConstantAgent, ClockworkAgent, SB3Agent, RecurrentPPOAgent #add anymore custom Agents (from train_agent.py) here as needed
+from user.train_agent import UserInputAgent, BasedAgent, ConstantAgent, ClockworkAgent, SB3Agent, RecurrentPPOAgent, CustomAgent #add anymore custom Agents (from train_agent.py) here as needed
 from user.my_agent import SubmittedAgent
 import pygame
 pygame.init()
 
-my_agent = UserInputAgent()
 
+# my_agent = CustomAgent(file_path="checkpoints/scared_new_agent/rl_model_1004400_steps.zip")
+# my_agent = CustomAgent(file_path="checkpoints/balanced_new_agent/clone_yuj_mlp_first_v12")
+# my_agent = CustomAgent(file_path="checkpoints/balanced_new_agent/rl_model_1004400_steps.zip")
+# my_agent = BasedAgent()
+# my_agent = UserInputAgent()
+my_agent = CustomAgent(file_path="checkpoints/based_starter/rl_model_0_steps.zip")
+# my_agent = SubmittedAgent()
 #Input your file path here in SubmittedAgent if you are loading a model:
-opponent = SubmittedAgent(file_path=None)
+# opponent = BasedAgent()
+opponent = ConstantAgent()
 
 match_time = 99999
 
